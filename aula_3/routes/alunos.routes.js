b1368450fe2bcf1499514fa3a64c42a03993addc
VITOR ALEXANDRE DOMINGUES OLIVEIRA <08919133603@pedag.local>
2025-08-28 14:38:06 -0300

    Continuação aula 3

--- aula_3/routes/alunos.routes.js
+++ aula_3/routes/alunos.routes.js
@@ -16,10 +16,62 @@ router.post('/', async (req, res, next) => {
     }
 });
 
-//GET
+//GET (kista de registros)
+router.get('/', async (req,res, next) => {
+    try {
+        const { nome }  = req.query;
+        const filtro = {};
+        if(nome){
+            filtro.nome = new RegExp(nome ,"i");
+        }
+        const alunos = await Aluno.find(filtro).sort({createdAt: -1});
+        return res.json(alunos);
+    } catch (error) {
+        next(error);
+    }
+});
+
+//GET (unico registro)
+
+router.get('/:id', async (req, res, next) => {
+    try {
+        const aluno = await Aluno.findById(req.params.id);
+        if(!aluno){
+            return res.status(404).json({erro: "Registro não encontrado"});
+        }
+        return res.json(aluno)
+    } catch (error) {
+        next(error);
+    }
+});
+
 
 //UPDATE
-
+router.put('/:id', async (req,res,next) => {
+    try {
+        const aluno = await Aluno.findByIdAndUpdate(req.params.id, req.body,{
+            new: true,
+            runValidators: true
+        });
+        if(!aluno){
+            return res.status(404).json({erro: "Aluno não encontrado"});
+        }
+        return res.json(aluno)
+    } catch (error) {
+        next(error)
+    }
+});
 //DELETE
+router.delete('/:id', async (req,res,next)=> {
+    try {
+        const aluno = await Aluno.findByIdAndDelete(req.params.id);
+        if(!aluno){
+            return res.status(404).json({erro: "Aluno não encontrado com esse id "});
+        }
+        return res.status(204).json({sucesso: "Usuario excluido com sucesso !!!"}).send().json({sucesso: "Usuario excluido com sucesso !!!"});
+    } catch (error) {
+        next(error);
+    }
+});
 
 module.exports = router;
